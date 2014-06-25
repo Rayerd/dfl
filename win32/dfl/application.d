3b0e4a96176e06d609a9904aeefb68564c5011f2
qchikara <jiki@red.email.ne.jp>
2014-06-25 23:56:42 +0900

    DFL64: Fix application manifest
    Change [processorArchitecture] value from X86 to N/A(= *).
    This is an important fix of the application manifest, which is
    referenced if a DFL application calls an initialization method,
    Application#enableVisualStyles().
    This does NOT make any change on Win32 programs.

--- win32/dfl/application.d
+++ win32/dfl/application.d
@@ -185,7 +185,7 @@ final class Application // docmain
 							`type="win32" `
 							`name="Microsoft.Windows.Common-Controls" `
 							`version="6.0.0.0" `
-							`processorArchitecture="X86" `
+							`processorArchitecture="*" `
 							`publicKeyToken="6595b64144ccf1df" `
 							`language="*" `
 						`/>` "\r\n"
