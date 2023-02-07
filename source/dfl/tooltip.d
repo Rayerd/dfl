f69baae4c9e50981e48448f4d3ef8a07389f12b7
haru-s <rayerd.wiz@gmail.com>
2023-02-07 16:29:44 +0900

    Update ToolTip - Add isBalloon, stripAmpersands, toolTipIcon, toolTipTitle, useAnimation and useFading

--- source/dfl/tooltip.d
+++ source/dfl/tooltip.d
@@ -6,50 +6,88 @@
 module dfl.tooltip;
 
 
-private import dfl.internal.dlib, dfl.internal.clib;
+private import dfl.application;
+private import dfl.base;
+private import dfl.control;
 
-private import dfl.control, dfl.base, dfl.application, dfl.internal.winapi,
-	dfl.internal.utf;
+private import dfl.internal.dlib;
+private import dfl.internal.clib;
+private import dfl.internal.utf;
+
+// private import dfl.internal.winapi; // Don't use
+private import core.sys.windows.windows;
+private import core.sys.windows.commctrl;
+
+
+///
+enum ToolTipIcon
+{
+	NONE = 0,
+	INFO = 1,
+	WARNING = 2,
+	ERROR = 3,
+	INFO_LARGE = 4,
+	WARNING_LARGE = 5,
+	ERROR_LARGE = 6,
+}
 
 
 ///
 class ToolTip // docmain
 {
-	package this(DWORD style)
+private:
+	static if (dfl.internal.utf.useUnicode)
+		enum TOOLTIPS_CLASS = "tooltips_class32"w;
+	else
+		enum TOOLTIPS_CLASS = "tooltips_class32";
+	enum size_t MAX_TIP_TEXT_LENGTH = 2045;
+	
+	HWND _hwtt; // Tooltip control handle.
+	bool _active = true;
+	ToolTipIcon _icon = ToolTipIcon.NONE;
+	Dstring _title;
+
+
+package:
+	this(DWORD style)
 	{
 		_initCommonControls(ICC_TREEVIEW_CLASSES); // Includes tooltip.
 		
-		hwtt = CreateWindowExA(WS_EX_TOPMOST | WS_EX_TOOLWINDOW, _TOOLTIPS_CLASSA.ptr,
+		_hwtt = CreateWindowEx(WS_EX_TOPMOST | WS_EX_TOOLWINDOW, TOOLTIPS_CLASS.ptr,
 			"", style, 0, 0, 50, 50, null, null, null, null);
-		if(!hwtt)
+		if(!_hwtt)
 			throw new DflException("Unable to create tooltip");
+
+		SendMessage(_hwtt, TTM_SETMAXTIPWIDTH, 0, int.max); // Support multi line.
 	}
 	
 	
+public:
 	this()
 	{
-		this(cast(DWORD)WS_POPUP);
+		enum TTS_USEVISUALSTYLE = 0x100;
+		this(cast(DWORD)(WS_POPUP | TTS_NOPREFIX));
 	}
 	
 	
 	~this()
 	{
 		removeAll(); // Fixes ref count.
-		DestroyWindow(hwtt);
+		DestroyWindow(_hwtt);
 	}
 	
 	
 	///
 	final @property HWND handle() // getter
 	{
-		return hwtt;
+		return _hwtt;
 	}
 	
 	
 	///
 	final @property void active(bool byes) // setter
 	{
-		SendMessageA(hwtt, TTM_ACTIVATE, byes, 0); // ?
+		SendMessage(_hwtt, TTM_ACTIVATE, byes, 0);
 		_active = byes;
 	}
 	
@@ -64,7 +102,7 @@ class ToolTip // docmain
 	// Sets autoPopDelay, initialDelay and reshowDelay.
 	final @property void automaticDelay(DWORD ms) // setter
 	{
-		SendMessageA(hwtt, TTM_SETDELAYTIME, TTDT_AUTOMATIC, ms);
+		SendMessage(_hwtt, TTM_SETDELAYTIME, TTDT_AUTOMATIC, ms);
 	}
 	
 	/+
@@ -78,7 +116,7 @@ class ToolTip // docmain
 	///
 	final @property void autoPopDelay(DWORD ms) // setter
 	{
-		SendMessageA(hwtt, TTM_SETDELAYTIME, TTDT_AUTOPOP, ms);
+		SendMessage(_hwtt, TTM_SETDELAYTIME, TTDT_AUTOPOP, ms);
 	}
 	
 	/+
@@ -92,7 +130,7 @@ class ToolTip // docmain
 	///
 	final @property void initialDelay(DWORD ms) // setter
 	{
-		SendMessageA(hwtt, TTM_SETDELAYTIME, TTDT_INITIAL, ms);
+		SendMessage(_hwtt, TTM_SETDELAYTIME, TTDT_INITIAL, ms);
 	}
 	
 	/+
@@ -106,7 +144,7 @@ class ToolTip // docmain
 	///
 	final @property void reshowDelay(DWORD ms) // setter
 	{
-		SendMessageA(hwtt, TTM_SETDELAYTIME, TTDT_RESHOW, ms);
+		SendMessage(_hwtt, TTS_BALLOON, 0, 0);
 	}
 	
 	/+
@@ -118,10 +156,154 @@ class ToolTip // docmain
 	
 	
 	///
+	final @property void isBalloon(bool byes) // setter
+	{
+		LONG wl;
+		wl = GetWindowLongPtr(_hwtt, GWL_STYLE).toI32;
+		if(byes)
+		{
+			if(wl & TTS_BALLOON)
+				return;
+			wl |= TTS_BALLOON;
+		}
+		else
+		{
+			if(!(wl & TTS_BALLOON))
+				return;
+			wl &= ~TTS_BALLOON;
+		}
+		SetWindowLongPtr(_hwtt, GWL_STYLE, wl);
+	}
+	
+	/// ditto
+	final @property bool isBalloon() // getter
+	{
+		return (GetWindowLongPtr(_hwtt, GWL_STYLE) & TTS_BALLOON) != 0;
+	}
+	
+
+	///
+	final @property void stripAmpersands(bool byes) // setter
+	{
+		LONG wl;
+		wl = GetWindowLongPtr(_hwtt, GWL_STYLE).toI32;
+		if(!byes)
+		{
+			if(wl & TTS_NOPREFIX)
+				return;
+			wl |= TTS_NOPREFIX;
+		}
+		else
+		{
+			if(!(wl & TTS_NOPREFIX))
+				return;
+			wl &= ~TTS_NOPREFIX;
+		}
+		SetWindowLongPtr(_hwtt, GWL_STYLE, wl);
+	}
+	
+	/// ditto
+	final @property bool stripAmpersands() // getter
+	{
+		return (GetWindowLongPtr(_hwtt, GWL_STYLE) & TTS_NOPREFIX) == 0;
+	}
+	
+
+	///
+	final @property void toolTipIcon(ToolTipIcon icon) // setter
+	{
+		_icon = icon;
+		static if (dfl.internal.utf.useUnicode)
+			SendMessage(_hwtt, TTM_SETTITLE, cast(WPARAM)_icon, cast(LPARAM)toUnicodez(_title));
+		else
+			SendMessage(_hwtt, TTM_SETTITLE, cast(WPARAM)_icon, cast(LPARAM)toAnsiz(_title));
+	}
+	
+	/// ditto
+	final @property ToolTipIcon toolTipIcon() // getter
+	{
+		return _icon;
+	}
+	
+	
+	///
+	// The maximum length of a title is 99 characters.
+	// If this property contains a string longer then 99 characters, no title will be displayed.
+	final @property void toolTipTitle(Dstring title) // setter
+	{
+		_title = title;
+		static if (dfl.internal.utf.useUnicode)
+			SendMessage(_hwtt, TTM_SETTITLE, cast(WPARAM)_icon, cast(LPARAM)toUnicodez(_title));
+		else
+			SendMessage(_hwtt, TTM_SETTITLE, cast(WPARAM)_icon, cast(LPARAM)toAnsiz(_title));
+	}
+	
+	/// ditto
+	final @property Dstring toolTipTitle() // getter
+	{
+		return _title;
+	}
+	
+	
+	///
+	final @property void useAnimation(bool byes) // setter
+	{
+		LONG wl;
+		wl = GetWindowLongPtr(_hwtt, GWL_STYLE).toI32;
+		if(!byes)
+		{
+			if(wl & TTS_NOANIMATE)
+				return;
+			wl |= TTS_NOANIMATE;
+		}
+		else
+		{
+			if(!(wl & TTS_NOANIMATE))
+				return;
+			wl &= ~TTS_NOANIMATE;
+		}
+		SetWindowLongPtr(_hwtt, GWL_STYLE, wl);
+	}
+	
+	/// ditto
+	final @property bool useAnimation() // getter
+	{
+		return (GetWindowLongPtr(_hwtt, GWL_STYLE) & TTS_NOANIMATE) == 0;
+	}
+	
+
+	///
+	final @property void useFading(bool byes) // setter
+	{
+		LONG wl;
+		wl = GetWindowLongPtr(_hwtt, GWL_STYLE).toI32;
+		if(!byes)
+		{
+			if(wl & TTS_NOFADE)
+				return;
+			wl |= TTS_NOFADE;
+		}
+		else
+		{
+			if(!(wl & TTS_NOFADE))
+				return;
+			wl &= ~TTS_NOFADE;
+		}
+		SetWindowLongPtr(_hwtt, GWL_STYLE, wl);
+	}
+	
+	/// ditto
+	final @property bool useFading() // getter
+	{
+		return (GetWindowLongPtr(_hwtt, GWL_STYLE) & TTS_NOFADE) == 0;
+	}
+
+
+	///
 	final @property void showAlways(bool byes) // setter
 	{
 		LONG wl;
-		wl = GetWindowLongPtrA(hwtt, GWL_STYLE).toI32;
+		wl = GetWindowLongPtr(_hwtt, GWL_STYLE).toI32;
 		if(byes)
 		{
 			if(wl & TTS_ALWAYSTIP)
@@ -134,13 +316,13 @@ class ToolTip // docmain
 				return;
 			wl &= ~TTS_ALWAYSTIP;
 		}
-		SetWindowLongPtrA(hwtt, GWL_STYLE, wl);
+		SetWindowLongPtr(_hwtt, GWL_STYLE, wl);
 	}
 	
 	/// ditto
 	final @property bool showAlways() // getter
 	{
-		return (GetWindowLongPtrA(hwtt, GWL_STYLE) & TTS_ALWAYSTIP) != 0;
+		return (GetWindowLongPtr(_hwtt, GWL_STYLE) & TTS_ALWAYSTIP) != 0;
 	}
 	
 	
@@ -148,11 +330,11 @@ class ToolTip // docmain
 	// Remove all tooltip text associated with this instance.
 	final void removeAll()
 	{
-		TOOLINFOA tool;
-		tool.cbSize = TOOLINFOA.sizeof;
-		while(SendMessageA(hwtt, TTM_ENUMTOOLSA, 0, cast(LPARAM)&tool))
+		TOOLINFO tool;
+		tool.cbSize = TOOLINFO.sizeof;
+		while(SendMessage(_hwtt, TTM_ENUMTOOLS, 0, cast(LPARAM)&tool))
 		{
-			SendMessageA(hwtt, TTM_DELTOOLA, 0, cast(LPARAM)&tool);
+			SendMessage(_hwtt, TTM_DELTOOL, 0, cast(LPARAM)&tool);
 			Application.refCountDec(cast(void*)this);
 		}
 	}
@@ -163,13 +345,13 @@ class ToolTip // docmain
 	final Dstring getToolTip(Control ctrl)
 	{
 		Dstring result;
-		TOOLINFOA tool;
-		tool.cbSize = TOOLINFOA.sizeof;
+		TOOLINFO tool;
+		tool.cbSize = TOOLINFO.sizeof;
 		tool.uFlags = TTF_IDISHWND;
 		tool.hwnd = ctrl.handle;
 		tool.uId = cast(UINT)ctrl.handle;
 		
-		if(dfl.internal.utf.useUnicode)
+		static if(dfl.internal.utf.useUnicode)
 		{
 			tool.lpszText = cast(typeof(tool.lpszText))dfl.internal.clib.malloc((MAX_TIP_TEXT_LENGTH + 1) * wchar.sizeof);
 			if(!tool.lpszText)
@@ -177,7 +359,7 @@ class ToolTip // docmain
 			scope(exit)
 				dfl.internal.clib.free(tool.lpszText);
 			tool.lpszText[0 .. 2] = 0;
-			SendMessageA(hwtt, TTM_GETTEXTW, 0, cast(LPARAM)&tool);
+			SendMessage(_hwtt, TTM_GETTEXT, 0, cast(LPARAM)&tool);
 			if(!(cast(wchar*)tool.lpszText)[0])
 				result = null;
 			else
@@ -191,7 +373,7 @@ class ToolTip // docmain
 			scope(exit)
 				dfl.internal.clib.free(tool.lpszText);
 			tool.lpszText[0] = 0;
-			SendMessageA(hwtt, TTM_GETTEXTA, 0, cast(LPARAM)&tool);
+			SendMessage(_hwtt, TTM_GETTEXT, 0, cast(LPARAM)&tool);
 			if(!tool.lpszText[0])
 				result = null;
 			else
@@ -200,7 +382,8 @@ class ToolTip // docmain
 		return result;
 	}
 	
-	/// ditto
+	
+	///
 	final void setToolTip(Control ctrl, Dstring text)
 	in
 	{
@@ -216,19 +399,19 @@ class ToolTip // docmain
 	}
 	do
 	{
-		TOOLINFOA tool;
-		tool.cbSize = TOOLINFOA.sizeof;
-		tool.uFlags = TTF_IDISHWND;
+		TOOLINFO tool;
+		tool.cbSize = TOOLINFO.sizeof;
+		tool.uFlags = TTF_IDISHWND | TTF_PARSELINKS;
 		tool.hwnd = ctrl.handle;
 		tool.uId = cast(UINT)ctrl.handle;
 		
 		if(!text.length)
 		{
-			if(SendMessageA(hwtt, TTM_GETTOOLINFOA, 0, cast(LPARAM)&tool))
+			if(SendMessage(_hwtt, TTM_GETTOOLINFO, 0, cast(LPARAM)&tool))
 			{
 				// Remove.
 				
-				SendMessageA(hwtt, TTM_DELTOOLA, 0, cast(LPARAM)&tool);
+				SendMessage(_hwtt, TTM_DELTOOL, 0, cast(LPARAM)&tool);
 				
 				Application.refCountDec(cast(void*)this);
 			}
@@ -239,19 +422,19 @@ class ToolTip // docmain
 		if(text.length > MAX_TIP_TEXT_LENGTH)
 			text = text[0 .. MAX_TIP_TEXT_LENGTH];
 		
-		if(SendMessageA(hwtt, TTM_GETTOOLINFOA, 0, cast(LPARAM)&tool))
+		if(SendMessage(_hwtt, TTM_GETTOOLINFO, 0, cast(LPARAM)&tool))
 		{
 			// Update.
-			
-			if(dfl.internal.utf.useUnicode)
+
+			static if(dfl.internal.utf.useUnicode)
 			{
 				tool.lpszText = cast(typeof(tool.lpszText))toUnicodez(text);
-				SendMessageA(hwtt, TTM_UPDATETIPTEXTW, 0, cast(LPARAM)&tool);
+				SendMessage(_hwtt, TTM_UPDATETIPTEXT, 0, cast(LPARAM)&tool);
 			}
 			else
 			{
 				tool.lpszText = cast(typeof(tool.lpszText))unsafeAnsiz(text);
-				SendMessageA(hwtt, TTM_UPDATETIPTEXTA, 0, cast(LPARAM)&tool);
+				SendMessage(_hwtt, TTM_UPDATETIPTEXT, 0, cast(LPARAM)&tool);
 			}
 		}
 		else
@@ -267,28 +450,19 @@ class ToolTip // docmain
 			+/
 			tool.uFlags |= TTF_SUBCLASS; // Not a good idea ?
 			LRESULT lr;
-			if(dfl.internal.utf.useUnicode)
+			static if(dfl.internal.utf.useUnicode)
 			{
 				tool.lpszText = cast(typeof(tool.lpszText))toUnicodez(text);
-				lr = SendMessageA(hwtt, TTM_ADDTOOLW, 0, cast(LPARAM)&tool);
+				lr = SendMessage(_hwtt, TTM_ADDTOOL, 0, cast(LPARAM)&tool);
 			}
 			else
 			{
 				tool.lpszText = cast(typeof(tool.lpszText))unsafeAnsiz(text);
-				lr = SendMessageA(hwtt, TTM_ADDTOOLA, 0, cast(LPARAM)&tool);
+				lr = SendMessage(_hwtt, TTM_ADDTOOL, 0, cast(LPARAM)&tool);
 			}
 			
 			if(lr)
 				Application.refCountInc(cast(void*)this);
 		}
 	}
-	
-	
-	private:
-	enum _TOOLTIPS_CLASSA = "tooltips_class32";
-	enum size_t MAX_TIP_TEXT_LENGTH = 2045;
-	
-	HWND hwtt; // Tooltip control handle.
-	bool _active = true;
 }
-
